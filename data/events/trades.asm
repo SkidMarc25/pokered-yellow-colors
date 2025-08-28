c380ec91545386c8a157a5960b92346535a0daf1
SkidMarc25 <marcstuts08@gmail.com>
2025-08-28 11:23:10 -0500

    Update trades.asm
    Updated Trades

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -14,14 +14,14 @@ TradeMons:
 	; "went and evolved" and also changed the trades to match. English
 	; Red/Blue uses the original JP Red/Green trades but with the JP Blue
 	; post-trade text.
-	npctrade VOLTORB,    KOFFING,   TRADE_DIALOGSET_CASUAL,    "CHUCKY" ; Route 12 Gate
+	npctrade VOLTORB,    KOFFING,   TRADE_DIALOGSET_CASUAL,    "CHUNKY" ; Route 12 Gate
 	npctrade ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL" ; Route 2 House
-	npctrade JIGGLYPUFF, SANDSHREW, TRADE_DIALOGSET_HAPPY,     "DUSTY" ; unused add to Museum
-	npctrade PONYTA,     HORSEA,    TRADE_DIALOGSET_CASUAL,    "DRAKE" ; Cinnabar Labs - Move somewhere else
+	npctrade JIGGLYPUFF, SANDSHREW, TRADE_DIALOGSET_HAPPY,     "DUSTY" ; unused - add to Museum
+	npctrade PONYTA,     HORSEA,    TRADE_DIALOGSET_CASUAL,    "DRAKE" ; Cinnabar Labs - Move somewhere else - Route 10
 	npctrade SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "DUX" ; Vermilion City
 	npctrade SLOWPOKE,   LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC" ; Route 18 Gate
-	npctrade POLIWAG,    SEEL,      TRADE_DIALOGSET_EVOLUTION, "MARINA" ; Cerulean City
-	npctrade GRAVELER,   MACHOKE,   TRADE_DIALOGSET_EVOLUTION, "RICKY" ; Cinnabar Labs - Move somewhere else
+	npctrade POLIWAG,    SMOOCHUM,  TRADE_DIALOGSET_EVOLUTION, "MARINA" ; Cerulean City
+	npctrade GRAVELER,   MAGCARGO,  TRADE_DIALOGSET_EVOLUTION, "ESCARGOON" ; Cinnabar Labs - Move somewhere else
 	npctrade MUK,        GOLDUCK,   TRADE_DIALOGSET_HAPPY,     "QUACKERS" ; Cinnabar Labs
-	npctrade NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "SPOT" ; Route 5 Underground ENtrance
+	npctrade NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "SPOT" ; Route 5 Underground Entrance
 	assert_table_length NUM_NPC_TRADES
