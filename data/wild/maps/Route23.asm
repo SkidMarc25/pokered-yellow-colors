5632aec0b23158b6ad604b1517f05840a5c73d14
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 10:34:02 -0500

    Update Route23.asm

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,16 +1,16 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 26, EKANS
+	db 26, RHYHORN
 ENDC
 IF DEF(_BLUE)
 	db 26, SANDSHREW
 ENDC
 	db 33, DITTO
-	db 26, SPEAROW
-	db 38, FEAROW
-	db 38, DITTO
-	db 38, FEAROW
+	db 26, SKARMORY
+	db 38, HOUNDOUR
+	db 38, PERSIAN
+	db 38, SANDSLASH
 IF DEF(_RED)
 	db 41, ARBOK
 ENDC
@@ -18,8 +18,8 @@ IF DEF(_BLUE)
 	db 41, SANDSLASH
 ENDC
 	db 43, DITTO
-	db 41, FEAROW
-	db 43, FEAROW
+	db 41, SKARMORY
+	db 43, SKARMORY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
