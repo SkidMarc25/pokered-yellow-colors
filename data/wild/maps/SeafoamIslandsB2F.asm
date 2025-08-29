f18d176431d6980abd7da6f49c0b938ae3795c3c
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 11:04:09 -0500

    Update SeafoamIslandsB2F.asm

--- data/wild/maps/SeafoamIslandsB2F.asm
+++ data/wild/maps/SeafoamIslandsB2F.asm
@@ -2,15 +2,16 @@ SeafoamIslandsB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 30, SEEL
 IF DEF(_RED)
-	db 30, SLOWPOKE
-	db 32, SEEL
-	db 32, SLOWPOKE
-	db 28, HORSEA
 	db 30, STARYU
-	db 30, HORSEA
-	db 28, SHELLDER
-	db 30, GOLBAT
-	db 37, SLOWBRO
+	db 30, SWINUB
+	db 32, SHELLDER
+	db 32, HORSEA
+	db 28, SLOWPOKE
+	db 30, SEEL
+	db 30, SLOWPOKE
+	db 28, SLOWBRO
+	db 38, DEWGONG
+	db 37, KINGLER
 ENDC
 IF DEF(_BLUE)
 	db 30, PSYDUCK
