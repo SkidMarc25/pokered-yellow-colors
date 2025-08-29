a3437e906122dc9ca7b01801989f7c03a4da3d66
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 10:28:39 -0500

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,14 +1,14 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 26, DITTO
-	db 23, PIDGEY
+	db 24, DODUO
+	db 26, PSYDUCK
+	db 23, PONYTA
 	db 26, VENONAT
-	db 22, ODDISH
+	db 22, VOLTORB
 	db 28, VENONAT
-	db 26, ODDISH
-	db 30, GLOOM
+	db 26, DITTO
+	db 30, DODRIO
 ENDC
 IF DEF(_BLUE)
 	db 24, BELLSPROUT
@@ -20,8 +20,8 @@ IF DEF(_BLUE)
 	db 26, BELLSPROUT
 	db 30, WEEPINBELL
 ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
+	db 28, DITTO
+	db 30, RAPIDASH
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
