0bc5d51d6da00cb3220bca0862f0a4a67d392140
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 09:57:56 -0500

    Update PokemonTower5F.asm

--- data/wild/maps/PokemonTower5F.asm
+++ data/wild/maps/PokemonTower5F.asm
@@ -1,13 +1,13 @@
 PokemonTower5FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 20, GASTLY
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
+	db 21, VULPIX
+	db 22, MISDREAVUS
+	db 23, CUBONE
 	db 19, GASTLY
-	db 18, GASTLY
+	db 18, MISDREAVUS
 	db 25, HAUNTER
-	db 20, CUBONE
+	db 20, VULPIX
 	db 22, CUBONE
 	db 24, GASTLY
 	end_grass_wildmons
