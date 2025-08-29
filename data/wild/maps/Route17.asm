3bfb746fd78f2225b5854d40318cb47e97468e60
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 10:20:19 -0500

    Update Route17.asm

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -1,14 +1,14 @@
 Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
+	db 20, PONYTA
+	db 22, DODUO
 	db 25, RATICATE
-	db 24, DODUO
+	db 24, SLUGMA
 	db 27, RATICATE
-	db 26, DODUO
+	db 26, PONYTA
 	db 28, DODUO
 	db 29, RATICATE
-	db 25, FEAROW
+	db 25, THIEVUL
 	db 27, FEAROW
 	end_grass_wildmons
 
