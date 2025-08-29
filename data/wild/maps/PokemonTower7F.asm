88317328c3f6647b6bf1d516bf0c3e6e42cd224c
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 09:58:30 -0500

    Update PokemonTower7F.asm

--- data/wild/maps/PokemonTower7F.asm
+++ data/wild/maps/PokemonTower7F.asm
@@ -1,15 +1,15 @@
 PokemonTower7FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
 	db 20, GASTLY
-	db 28, HAUNTER
+	db 21, MISDREAVUS
+	db 22, MISDREAVUS
+	db 23, CUBONE
+	db 19, GASTLY
+	db 18, MISDREAVUS
+	db 25, HAUNTER
+	db 20, VULPIX
 	db 22, CUBONE
-	db 24, CUBONE
-	db 28, HAUNTER
-	db 30, HAUNTER
+	db 24, GASTLY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
