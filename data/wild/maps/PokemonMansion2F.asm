efcee268cb4df907077fd3903fe63b1215d577de
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 10:56:22 -0500

    Update PokemonMansion2F.asm

--- data/wild/maps/PokemonMansion2F.asm
+++ data/wild/maps/PokemonMansion2F.asm
@@ -1,16 +1,16 @@
 PokemonMansion2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 32, GROWLITHE
-	db 34, KOFFING
-	db 34, KOFFING
-	db 30, PONYTA
-	db 30, KOFFING
-	db 32, PONYTA
+	db 32, KOFFING
+	db 30, HOUNDOUR
+	db 34, MAGBY
+	db 30, VULPIX
+	db 34, GROWLITHE
+	db 32, DITTO
 	db 30, GRIMER
-	db 28, PONYTA
-	db 39, WEEZING
-	db 37, MUK
+	db 43, RATICATE
+	db 37, WEEZING
+	db 39, MUK
 ENDC
 IF DEF(_BLUE)
 	db 32, VULPIX
