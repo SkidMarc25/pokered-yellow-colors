d319cf68f3b1a993279fe9def7551291f0477cae
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 09:58:13 -0500

    Update PokemonTower6F.asm

--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -1,15 +1,15 @@
 PokemonTower6FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
 	db 20, GASTLY
+	db 21, VULPIX
+	db 22, MISDREAVUS
+	db 23, CUBONE
 	db 19, GASTLY
-	db 26, HAUNTER
+	db 18, MISDREAVUS
+	db 25, HAUNTER
+	db 20, VULPIX
 	db 22, CUBONE
-	db 24, CUBONE
-	db 28, HAUNTER
+	db 24, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
