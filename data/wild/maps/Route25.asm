685e8f1b4d8e4abca85f7f968446e8d2c678084a
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 09:08:55 -0500

    Update Route25.asm

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,16 +1,16 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
-	db 13, PIDGEY
-	db 12, ODDISH
-	db 13, ODDISH
+	db  8, PIDGEY
+	db  9, ODDISH
+	db 13, PSYDUCK
+	db 12, BELLSPROUT
+	db 13, VENONAT
 	db 12, ABRA
-	db 14, ODDISH
+	db 14, VENONAT
 	db 10, ABRA
 	db  7, METAPOD
-	db  8, CATERPIE
+	db  8, BUTTERFREE
 ENDC
 IF DEF(_BLUE)
 	db  8, CATERPIE
