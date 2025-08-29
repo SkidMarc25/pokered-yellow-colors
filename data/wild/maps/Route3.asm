d23aa49d0769bd91f8c4b94481e5bea37eb08d7b
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 08:57:57 -0500

    Update Route3.asm

--- data/wild/maps/Route3.asm
+++ data/wild/maps/Route3.asm
@@ -2,11 +2,11 @@ Route3WildMons:
 	def_grass_wildmons 20 ; encounter rate
 	db  6, PIDGEY
 	db  5, SPEAROW
-	db  7, PIDGEY
-	db  6, SPEAROW
+	db  7, NICKIT
+	db  6, EKANS
 	db  7, SPEAROW
-	db  8, PIDGEY
-	db  8, SPEAROW
+	db  8, NICKIT
+	db  8, EKANS
 	db  3, JIGGLYPUFF
 	db  5, JIGGLYPUFF
 	db  7, JIGGLYPUFF
