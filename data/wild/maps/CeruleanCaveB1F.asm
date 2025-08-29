2f7c03143c2a884a6d7b5025f0023080f4b9f9c0
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 10:53:40 -0500

    Update CeruleanCaveB1F.asm

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -1,20 +1,20 @@
 CeruleanCaveB1FWildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 55, RHYDON
-	db 55, MAROWAK
+	db 55, GOLDUCK
+	db 55, PILOSWINE
 	db 55, ELECTRODE
 	db 64, CHANSEY
-	db 64, PARASECT
-	db 64, RAICHU
+	db 64, MAGCARGO
+	db 64, DUGTRIO
 IF DEF(_RED)
 	db 57, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 57, SANDSLASH
 ENDC
-	db 65, DITTO
-	db 63, DITTO
-	db 67, DITTO
+	db 65, MAGMORTAR
+	db 63, ELECTIVIRE
+	db 67, CLEFABLE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
