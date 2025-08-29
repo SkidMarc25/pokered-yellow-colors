e2d14851926482310328ff8a944b8e74b98d93bc
SkidMarc25 <marcstuts08@gmail.com>
2025-08-29 09:00:32 -0500

    Update Route10.asm

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -4,11 +4,11 @@ Route10WildMons:
 	db 16, SPEAROW
 	db 14, VOLTORB
 IF DEF(_RED)
-	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
-	db 17, VOLTORB
-	db 17, SPEAROW
+	db 11, PIKACHU
+	db 13, MAGNEMITE
+	db 15, KOFFING
+	db 17, ELEKID
+	db 17, HOUNDOUR
 	db 13, EKANS
 	db 17, EKANS
 ENDC
